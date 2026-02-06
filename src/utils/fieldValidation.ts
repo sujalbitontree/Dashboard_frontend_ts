542d027d13fd99e0c08e4c78eba0e969870cd74a
sujalbitontree <sujal.k@bitontree.com>
2026-02-06 12:38:39 +0530

    added forgot password functionality

--- src/utils/fieldValidation.ts
+++ src/utils/fieldValidation.ts
@@ -1,11 +1,15 @@
-import { SignupInput, signupSchema } from "./signupValidation";
+import { SignupInput, signupSchema } from './signupValidation'
 
-export const validateField = async (name: keyof SignupInput , value : string,allFormData: SignupInput)=>{
-   try {
-    const dataTovalidate = {...allFormData,[name]:value}
-    await signupSchema.validateAt(name,dataTovalidate)
-    return ""
-   } catch (error : any) {
+export const validateField = async (
+  name: keyof SignupInput,
+  value: string,
+  allFormData: SignupInput
+) => {
+  try {
+    const dataTovalidate = { ...allFormData, [name]: value }
+    await signupSchema.validateAt(name, dataTovalidate)
+    return ''
+  } catch (error: any) {
     return error.message
-   }
+  }
 }
